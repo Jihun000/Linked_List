4d68afaa492d5240d705f3ab863acef18e07adf5
Jh._.232 <81608240+Jihun000@users.noreply.github.com>
2022-01-13 14:20:24 +0900

    Create Update_First.h

---
+++ Update_First.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+
+struct _NODE {
+	struct _NODE* next;
+	int data;
+	int count;
+};
+
+int Count = 0;
+
+void print_Node(struct _NODE* Node);
+void First_index_insert_Node(struct _NODE* Node, int data);
+void First_index_delete_Node(struct _NODE* Node);
+void select_number_delete_node(struct _NODE* Node, int data);
